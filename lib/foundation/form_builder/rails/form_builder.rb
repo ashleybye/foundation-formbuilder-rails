8a05eccfa9c9c95dc4fe1da4f4bcb55d74ac2781
Ashley Bye <ashley.bye85@gmail.com>
2014-08-01 13:50:29 +0100

    Prevent error div inside label tags

--- lib/foundation/form_builder/rails/form_builder.rb
+++ lib/foundation/form_builder/rails/form_builder.rb
@@ -645,7 +645,7 @@ module Foundation
           def get_field_errors(method)
             # If we have any errors, return them and delete them from Rails
             # default object to prevent <div></div> inside <label></label>
-            options.errors.delete(method)
+            object.errors.delete(method)
           end
 
           # Return a class attribute containing the css error class for the label
